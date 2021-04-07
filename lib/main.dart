c5b7ce658b68c38cc841f246cc52bf3b89df0c29
Priyash Garg <priyash634@gmail.com>
2021-04-07 18:34:49 +0530

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('images/Sp_image.jpg'),
+      ),
+    );
+  }
+}
